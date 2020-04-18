ea72e1c119ae703a19e67d9dd2a12df31a9fddc4
william <191marquis@gmail.com>
2020-04-18 13:13:46 -0400

    [INIT] Do first try

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    """This sees if boxes can be unlocked given box 0 unlocked"""
+    unlocked = []
+    for i in range(0, len(boxes)):
+        unlocked.append(False)
+    unlocked[0] = True
+
+    todo = [0]
+    while len(todo) > 0:
+        j = todo.pop(0)
+        for k in boxes[j]:
+            if not unlocked[k]:
+                unlocked[k] = True
+                todo.append(k)
+
+    return all(unlocked)
---
+++ 0x00-lockboxes/README.md
@@ -0,0 +1,3 @@
+# This folder and file is the question:
+
+If you have n locked boxes in front of you and each box is numbered from 0 to n-1 (represented by a list of boxes, with each box represented as a list, and each index representing the number of the box), write a method that determines if all the boxes can be opened if only box #0 is open to start.
