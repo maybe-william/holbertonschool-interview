21a81af153bb60b0a493a7e6c97e22568848a9ec
william <191marquis@gmail.com>
2020-06-01 01:24:16 -0400

    [BUGFIX] Always count filesize even if incorrect code

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -12,21 +12,23 @@ if __name__ == "__main__":
 
     def print_stuff():
         """print file size and status code frequency"""
-        if (total_size > 0):
-            print('File size: {}'.format(total_size))
-            for i in sorted(code_nums.keys()):
-                j = code_nums.get(i)
-                print('{}: {}'.format(i, j))
+        print('File size: {}'.format(total_size))
+        for i in sorted(code_nums.keys()):
+            j = code_nums.get(i)
+            print('{}: {}'.format(i, j))
 
     try:
         for line in sys.stdin:
             valid_codes = "(200|301|400|401|403|404|405|500)"
             patt = re.compile("^.*\s+" + valid_codes + "\s+(\d+)\s*$")
             x = patt.match(line)
+            patt = re.compile("^.*\s+(\d+)\s*$")
+            y = patt.match(line)
+            if (y):
+                size = y[1]
+                total_size = total_size + int(size)
             if (x):
                 code = x[1]
-                size = x[2]
-                total_size = total_size + int(size)
                 code_nums[code] = code_nums.get(code, 0) + 1
             line_num = line_num + 1
             if line_num == 10:
