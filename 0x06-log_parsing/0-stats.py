600d63944fefe607d8064215e094d8a59402b6fe
william <191marquis@gmail.com>
2020-06-01 00:38:48 -0400

    [BUGFIX] Change SIGINT handling

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -10,11 +10,6 @@ if __name__ == "__main__":
     code_nums = {}
     line_num = 0
 
-    def sig_handler(sig, frame):
-        """catch the interrupt signal, print, and exit"""
-        print_stuff()
-        sys.exit(0)
-
     def print_stuff():
         """print file size and status code frequency"""
         print('File size: {}'.format(total_size))
@@ -22,15 +17,19 @@ if __name__ == "__main__":
             j = code_nums.get(i)
             print('{}: {}'.format(i, j))
 
-    signal.signal(signal.SIGINT, sig_handler)
-    for line in sys.stdin:
-        if line_num == 10:
-            line_num = 0
-            print_stuff()
-        patt = re.compile(".*\".*\"\s+(.*)\s+(.*)")
-        x = patt.match(line)
-        code = x[1]
-        size = x[2]
-        total_size = total_size + int(size)
-        code_nums[code] = code_nums.get(code, 0) + 1
-        line_num = line_num + 1
+    try:
+        for line in sys.stdin:
+            if line_num == 10:
+                line_num = 0
+                print_stuff()
+            patt = re.compile(".*\".*\"\s+(.*)\s+(.*)")
+            x = patt.match(line)
+            code = x[1]
+            size = x[2]
+            total_size = total_size + int(size)
+            code_nums[code] = code_nums.get(code, 0) + 1
+            line_num = line_num + 1
+    except KeyboardInterrupt:
+        print_stuff()
+        raise
+    print_stuff()
