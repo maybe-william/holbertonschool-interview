4e3c6c1bca805da25d247a0546a9cead4f2843fc
william <191marquis@gmail.com>
2020-06-01 01:41:19 -0400

    [BUGFIX] Switch back to using signal handling

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -10,6 +10,10 @@ if __name__ == "__main__":
     code_nums = {}
     line_num = 0
 
+    def sig_hand(sig, frame):
+        """handle sigint"""
+        print_stuff()
+
     def print_stuff():
         """print file size and status code frequency"""
         print('File size: {}'.format(total_size))
@@ -17,24 +21,21 @@ if __name__ == "__main__":
             j = code_nums.get(i)
             print('{}: {}'.format(i, j))
 
-    try:
-        for line in sys.stdin:
-            valid_codes = "(200|301|400|401|403|404|405|500)"
-            patt = re.compile("^.*\s+" + valid_codes + "\s+(\d+)\s*$")
-            x = patt.match(line)
-            patt = re.compile("^.*\s+(\d+)\s*$")
-            y = patt.match(line)
-            if (y):
-                size = y[1]
-                total_size = total_size + int(size)
-            if (x):
-                code = x[1]
-                code_nums[code] = code_nums.get(code, 0) + 1
-            line_num = line_num + 1
-            if line_num == 10:
-                line_num = 0
-                print_stuff()
-    except KeyboardInterrupt:
-        print_stuff()
-        raise
+    for line in sys.stdin:
+        valid_codes = "(200|301|400|401|403|404|405|500)"
+        patt = re.compile("^.*\s+" + valid_codes + "\s+(\d+)\s*$")
+        x = patt.match(line)
+        patt = re.compile("^.*\s+(\d+)\s*$")
+        y = patt.match(line)
+        if (y):
+            size = y[1]
+            total_size = total_size + int(size)
+        if (x):
+            code = x[1]
+            code_nums[code] = code_nums.get(code, 0) + 1
+        line_num = line_num + 1
+        if line_num == 10:
+            line_num = 0
+            print_stuff()
+        signal.signal(signal.SIGINT, sig_hand)
     print_stuff()
