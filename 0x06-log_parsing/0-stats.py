871670fbc10da2b46fe2ae721dde3af1c5a50fd4
william <191marquis@gmail.com>
2020-06-01 00:26:18 -0400

    [INIT] Do second try

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
 """This file reads stdin line by line and computes metrics"""
-
 import re
 import signal
 import sys
 
-total_size = 0
-code_nums = {}
-line_num = 0
-
-
-def sig_handler(sig, frame):
-    """catch the interrupt signal, print, and exit"""
-    print_stuff()
-    sys.exit(0)
-
-
-def print_stuff():
-    """print file size and status code frequency"""
-    print('File size: {}'.format(total_size))
-    for i in sorted(code_nums.keys()):
-        j = code_nums.get(i)
-        print('{}: {}'.format(i, j))
-
 
 if __name__ == "__main__":
+    total_size = 0
+    code_nums = {}
+    line_num = 0
+
+    def sig_handler(sig, frame):
+        """catch the interrupt signal, print, and exit"""
+        print_stuff()
+        sys.exit(0)
+
+    def print_stuff():
+        """print file size and status code frequency"""
+        print('File size: {}'.format(total_size))
+        for i in sorted(code_nums.keys()):
+            j = code_nums.get(i)
+            print('{}: {}'.format(i, j))
+
     signal.signal(signal.SIGINT, sig_handler)
     while (True):
         if line_num == 10:
