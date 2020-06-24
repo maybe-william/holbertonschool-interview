f914faab614f10395ca9b188338df1696fb70b5a
william <191marquis@gmail.com>
2020-06-24 18:01:53 -0400

    [BUGFIX] Check if list is empty

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -27,7 +27,7 @@ def verify_byte(byte, style=1):
 
 def validUTF8(data):
     """Given an array of bytes, determine whether or not it's valid UTF8"""
-    if len(data) == 0 or data is None:
+    if data is None or len(data) == 0:
         return True
     numOfFiller = 0
     for byte in data:
